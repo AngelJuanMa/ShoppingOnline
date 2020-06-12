2db2c2888145482e2f673f48fa884f834d258edc
AngelJuanMa <angelcarballo66666@gmail.com>
2020-06-11 22:12:29 -0300

    Updates

---
+++ precache-manifest.626cb7c63dcfb11db98ac0476e8ea761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8db74ba142566da6f147239f6b1d3c",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "ee315e0ecaa77978637c",
+    "url": "/React/static/css/main.abed2cb2.chunk.css"
+  },
+  {
+    "revision": "0617b736d2361e0cc49c",
+    "url": "/React/static/js/2.91481ce3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.91481ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee315e0ecaa77978637c",
+    "url": "/React/static/js/main.4ae0b906.chunk.js"
+  },
+  {
+    "revision": "8d72952e4cd1f5a07071",
+    "url": "/React/static/js/runtime-main.3b84d0d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
