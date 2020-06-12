d3a3d3f6e78b0767939c18cb0d2d8c2a1a075d5a
AngelJuanMa <angelcarballo66666@gmail.com>
2020-06-11 22:14:19 -0300

    Updates

---
+++ precache-manifest.3c600f2fb658be6298dbe01a68e1771c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094184fd33c002070ce434eb6fd8684f",
+    "url": "/ShoppingOnline/index.html"
+  },
+  {
+    "revision": "c2de168ae37722237383",
+    "url": "/ShoppingOnline/static/css/main.abed2cb2.chunk.css"
+  },
+  {
+    "revision": "0617b736d2361e0cc49c",
+    "url": "/ShoppingOnline/static/js/2.91481ce3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingOnline/static/js/2.91481ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2de168ae37722237383",
+    "url": "/ShoppingOnline/static/js/main.c4ca8563.chunk.js"
+  },
+  {
+    "revision": "60ff3a28bdd71d8cdfae",
+    "url": "/ShoppingOnline/static/js/runtime-main.56707f3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShoppingOnline/static/media/logo.5d5d9eef.svg"
+  }
+]);
